f3d6c08682e8335396ad043c2c53b9939f348764
root <remi@yasaw.net>
2012-07-21 17:47:17 +0200

    first commit

---
+++ vztools.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+# -- vzsh : utilitaire de gestion de VM  _ Rémi J.
+
+
+CTIDS=($(vzlist -H -o veid | sed s/\ //g | tr '\n' ' '))
+
+# -- checkRequirements : Verify that all needed tools are presents
+checkRequirements()
+{
+   if [[ $EUID -ne 0 ]]; then
+       echo "This script must be run as root ! -- Error" 1>&2
+       exit 1
+   fi
+
+    tools=( vzlist sed grep )
+    for tool in ${tools[@]} 
+    do
+        if which $tool >/dev/null; then
+            echo -e "\t${tool} exists -- Ok"
+        else
+            echo -e "\t${tool} not found -- Error"
+            exit 1
+        fi
+    done
+}
+
+
+# -- updateCT : update containers
+updateCT()
+{
+    for CTID in ${CTIDS[@]}
+        do
+            echo "------- Updating ${CTID}"
+	    vzctl exec ${CTID} apt-get update
+            read -p "Press Enter to continue..."
+        done
+}
+
+# -- upgradeCT : upgrade containers
+upgradeCT()
+{
+     for CTID in ${CTIDS[@]}
+        do  
+            echo "- Updating ${CTID}"
+            vzctl exec ${CTID} apt-get upgrade
+            read -p "Press Enter to continue..."
+        done
+}
+
+# -- memoryCT : memory usage containers
+memoryCT()
+{
+     for CTID in ${CTIDS[@]}
+        do  
+            echo "-  Memory ${CTID}"
+            vzctl exec ${CTID} free -m
+        done
+}
+
+# -- versionCT : distribution version containers
+versionCT()
+{
+     for CTID in ${CTIDS[@]}
+        do  
+            echo -n "- CTID ${CTID}:  "
+            vzctl exec ${CTID} cat /etc/debian_version
+        done
+}
+
+# -- Main 
+echo 'Vzsh v0.1'
+echo '---------'
+checkRequirements
+
+case "$1" in
+  update)
+      echo "___Updating :___"
+      updateCT
+      ;;
+  upgrade)
+      echo "___Upgrading :___"
+      upgradeCT
+      ;;
+  memory)
+      echo "___Memory :___"
+      memoryCT
+      ;;
+  version)
+      echo "___version :___"
+      versionCT
+      ;;
+   *)
+      echo "Commandes disponibles: update upgrade memory"
+esac
+
